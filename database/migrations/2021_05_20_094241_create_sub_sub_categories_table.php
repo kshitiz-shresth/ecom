4a56a0e4505dde5926739bea45038ef900ae22cb
Kshitiz Shrestha <er.kshitizshrestha@gmail.com>
2021-05-20 15:42:21 +0545

    some changes

---
+++ database/migrations/2021_05_20_094241_create_sub_sub_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubSubCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sub_sub_categories', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('sub_category_id')->nullable();
+            $table->string('name');
+            $table->string('slug');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sub_sub_categories');
+    }
+}
